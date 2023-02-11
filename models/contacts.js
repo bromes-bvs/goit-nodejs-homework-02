8d83a2a6930463ccf76829b1d6767579e4a59b6c
Borys Supreme <bvs.bromes@gmail.com>
2023-02-11 04:33:42 +0200

    finished + add joi

--- models/contacts.js
+++ models/contacts.js
@@ -1,74 +1,72 @@
 const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "./contacts.json");
 
 const listContacts = async () => {
-  try {
-    const contactsList = await fs.readFile(contactsPath, "utf-8");
-    return JSON.parse(contactsList);
-  } catch (error) {
-    console.log(error.message);
-  }
+  const contactsList = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(contactsList);
 };
 
 const getContactById = async (contactId) => {
-  try {
-    const contactsList = await fs.readFile(contactsPath, "utf-8");
+  const contactsList = await fs.readFile(contactsPath, "utf-8");
 
-    const searchContact = JSON.parse(contactsList).find(
-      ({ id }) => id === contactId.toString()
-    );
-    if (!searchContact) {
-      return console.log("Sorry there is no contact with this id");
-    }
-    return searchContact;
-  } catch (error) {
-    console.log(error.message);
+  const searchContact = JSON.parse(contactsList).find(
+    ({ id }) => id === contactId.toString()
+  );
+  if (!searchContact) {
+    return null;
   }
+  return searchContact;
 };
 
 const removeContact = async (contactId) => {
-  try {
-    const contactsList = await fs.readFile(contactsPath, "utf-8");
-    const parsContacts = JSON.parse(contactsList);
-    const contactIndex = parsContacts.findIndex(
-      ({ id }) => id === contactId.toString()
-    );
-    if (contactIndex === -1) {
-      return console.log("Sorry there is no contact with this id");
-    }
-
-    const deleteContact = parsContacts.splice(contactIndex, 1);
-
-    await fs.writeFile(contactsPath, JSON.stringify(parsContacts, null, "\t"));
-    return deleteContact;
-  } catch (error) {
-    console.log(error.message);
+  const contactsList = await fs.readFile(contactsPath, "utf-8");
+  const parsContacts = JSON.parse(contactsList);
+  const contactIndex = parsContacts.findIndex(
+    ({ id }) => id === contactId.toString()
+  );
+  if (contactIndex === -1) {
+    return null;
   }
+
+  const deleteContact = parsContacts.splice(contactIndex, 1);
+
+  await fs.writeFile(contactsPath, JSON.stringify(parsContacts, null, "\t"));
+  return deleteContact;
 };
 
 const addContact = async (body) => {
-  try {
-    const { name, email, phone } = body;
-    const newContact = {
-      id: nanoid(4),
-      name: name,
-      email,
-      phone,
-    };
-    const contactsList = await fs.readFile(contactsPath, "utf-8");
-    const contacts = JSON.parse(contactsList);
-    contacts.push(newContact);
+  const { name, email, phone } = body;
+  const newContact = {
+    id: nanoid(4),
+    name: name,
+    email,
+    phone,
+  };
+  const contactsList = await fs.readFile(contactsPath, "utf-8");
+  const contacts = JSON.parse(contactsList);
+  contacts.push(newContact);
 
-    console.table(contacts);
-    return await fs.writeFile(
-      contactsPath,
-      JSON.stringify(contacts, null, "\t")
-    );
-  } catch (error) {
-    console.log(error.message);
-  }
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, "\t"));
+  return newContact;
 };
 
-const updateContact = async (contactId, body) => {};
+const updateContact = async (contactId, body) => {
+  const { name, email, phone } = body;
+  const contactsList = await fs.readFile(contactsPath, "utf-8");
+  const parsContacts = JSON.parse(contactsList);
+  const index = parsContacts.findIndex(({ id }) => id === contactId.toString());
+
+  if (index === -1) {
+    return null;
+  }
+  parsContacts[index] = { id: contactId, name, email, phone };
+
+  await fs.writeFile(contactsPath, JSON.stringify(parsContacts, null, "\t"));
+  return parsContacts[index];
+};
 
 module.exports = {
   listContacts,
