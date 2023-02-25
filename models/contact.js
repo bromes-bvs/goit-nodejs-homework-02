7c350faa0bf7ee748b167a7e3200d66fdd614499
Borys Supreme <bvs.bromes@gmail.com>
2023-02-26 00:18:20 +0200

    changed

--- models/contact.js
+++ models/contact.js
@@ -1,77 +1,49 @@
-const fs = require("fs").promises;
-const path = require("path");
-const { nanoid } = require("nanoid");
+const Joi = require("joi");
+const { Schema, model } = require("mongoose");
 
-const contactsPath = path.join(__dirname, "./contacts.json");
+const contactSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
-const listContacts = async () => {
-  const contactsList = await fs.readFile(contactsPath, "utf-8");
-  return JSON.parse(contactsList);
+contactSchema.post("save", (error, date, next) => {
+  error.status = 400;
+  next();
+});
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+const schemas = {
+  addSchema,
+  updateFavoriteSchema,
 };
 
-const getContactById = async (contactId) => {
-  const contactsList = await fs.readFile(contactsPath, "utf-8");
-
-  const searchContact = JSON.parse(contactsList).find(
-    ({ id }) => id === contactId.toString()
-  );
-  if (!searchContact) {
-    return null;
-  }
-  return searchContact;
-};
-
-const removeContact = async (contactId) => {
-  const contactsList = await fs.readFile(contactsPath, "utf-8");
-  const parsContacts = JSON.parse(contactsList);
-  const contactIndex = parsContacts.findIndex(
-    ({ id }) => id === contactId.toString()
-  );
-  if (contactIndex === -1) {
-    return null;
-  }
-
-  const deleteContact = parsContacts.splice(contactIndex, 1);
-
-  await fs.writeFile(contactsPath, JSON.stringify(parsContacts, null, "\t"));
-  return deleteContact;
-};
-
-const addContact = async (body) => {
-  const { name, email, phone } = body;
-  const newContact = {
-    id: nanoid(4),
-    name: name,
-    email,
-    phone,
-  };
-  const contactsList = await fs.readFile(contactsPath, "utf-8");
-  const contacts = JSON.parse(contactsList);
-  contacts.push(newContact);
-
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, "\t"));
-  return newContact;
-};
-
-const updateContact = async (contactId, body) => {
-  const { name, email, phone } = body;
-  const contactsList = await fs.readFile(contactsPath, "utf-8");
-  const parsContacts = JSON.parse(contactsList);
-  const index = parsContacts.findIndex(({ id }) => id === contactId.toString());
-
-  if (index === -1) {
-    return null;
-  }
-  parsContacts[index] = { id: contactId, name, email, phone };
-
-  await fs.writeFile(contactsPath, JSON.stringify(parsContacts, null, "\t"));
-  return parsContacts[index];
-};
+const Contact = model("contact", contactSchema);
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+  Contact,
+  schemas,
 };
