0a8e2ed51d458f0f03937ab2d61a6e1de38e3ec9
Borys Supreme <bvs.bromes@gmail.com>
2023-03-02 08:31:05 +0200

    start dev

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const ctrl = require("../../controllers/contacts");
 const isValideId = require("../../middlewares/isValidId");
+const auth = require("../../middlewares/auth");
 
 const router = express.Router();
 
-router.get("/", ctrl.getAll);
-router.get("/:contactId", isValideId, ctrl.getById);
-router.post("/", ctrl.addContact);
-router.delete("/:contactId", isValideId, ctrl.deleteContact);
-router.put("/:contactId", isValideId, ctrl.updateById);
-router.patch("/:contactId/favorite", isValideId, ctrl.updateStatusContact);
+router.get("/", auth, ctrl.getAll);
+router.get("/:contactId", auth, isValideId, ctrl.getById);
+router.post("/", auth, ctrl.addContact);
+router.delete("/:contactId", auth, isValideId, ctrl.deleteContact);
+router.put("/:contactId", auth, isValideId, ctrl.updateById);
+router.patch(
+  "/:contactId/favorite",
+  auth,
+  isValideId,
+  ctrl.updateStatusContact
+);
 
 module.exports = router;
